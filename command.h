25f41cc2c35d7e7620c792df2cdb1e6e46afafb6
喻明理 <ymlwww@users.noreply.github.com>
2017-12-02 22:32:14 +0800

    Create command.h

---
+++ command.h
@@ -0,0 +1,11 @@
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
+
+
+/* Monitor Command Prompt */
+#define CONFIG_SYS_PROMPT "->"
+
+/* Buffer size for input from the Console */
+#define CONFIG_SYS_CBSIZE		256
+
+#endif
